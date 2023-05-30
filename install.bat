77e2973b9b45154c0fd0d5bd280465e8ee9ac41f
Synthetic <130696187+syntheticlol@users.noreply.github.com>
2023-05-30 20:08:13 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,8 +1,2 @@
-pip install shutil
-pip install discord.py
-pip install discord
-pip install requests
-pip install asyncio
-pip install concurrent.futures
-pip install pystyle
-pip install shutil
+pip install -r requirements.txt
+start main.py
